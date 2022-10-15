77d03a140569dc8821957da4dd5f2c3c9b2696fb
PranaliMohite0408 <pranalimohite482000@gmail.com>
2022-10-15 13:27:40 +0530

    TypeScript Program

---
+++ Function/function4.ts
@@ -0,0 +1,15 @@
+function main():void    //function Defination
+{
+    console.log("Inside Main");
+
+    fun();              //function call
+
+    console.log("End Main")
+}
+
+function fun():void     //function Defination
+{
+    console.log("Inside fun !!!!");
+}
+
+main();             
