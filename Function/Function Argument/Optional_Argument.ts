2fd0f372d7dea497c7e946a366f7c1420e215d36
PranaliMohite0408 <pranalimohite482000@gmail.com>
2022-10-16 10:03:47 +0530

    TypeScript Program

---
+++ Function/Function Argument/Optional_Argument.ts
@@ -0,0 +1,17 @@
+//Optional Argument
+
+function Demo(no1:number, no2?:number)
+{
+    console.log("Inside Demo!!!");
+    //console.log("Value of no1 : "+no1);
+    //console.log("Value of no2 : "+no2);
+
+    console.log("Value of no1 : "+no1)
+    if(no2 != undefined)
+    {
+        console.log("Value of no2 : "+no2);
+    }
+}
+
+Demo(10,11);    //no1=10, no2=11
+Demo(10);       //no1=10, no=undefine
