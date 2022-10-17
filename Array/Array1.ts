224927b8635c4663d04df35df18a7c816d1b6d30
PranaliMohite0408 <pranalimohite482000@gmail.com>
2022-10-17 21:20:56 +0530

    TypeScript Program

---
+++ Array/Array1.ts
@@ -0,0 +1,18 @@
+//int Arr[5]={10,20,30,40} //C,C++,Java,C#
+
+//Arr is a one dimension array which contains 5 elements each element is of type number.
+
+var Arr:number[] = [10,20,30,40,50];
+
+console.log("Length of Array is : "+Arr.length);
+
+console.log("First Element is : "+Arr[0]);
+console.log("Second Element of Array is : "+Arr[1]);
+
+console.log("Elements of Array are =>");
+var i:number=0;
+
+for(i=0;i<Arr.length;i++)
+{
+    console.log(Arr[i]);
+}
