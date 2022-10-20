8be1c5dc7697e7c47338474686f00cd2a0c45001
PranaliMohite0408 <pranalimohite482000@gmail.com>
2022-10-20 19:02:28 +0530

    TypeScript Program

---
+++ Tuple/tuple.ts
@@ -0,0 +1,16 @@
+//Tuple is collection of Elements having any type of data type
+
+var Arr = [10,"Hello",true,90,78];
+
+console.log("Length of Array is : "+Arr.length);
+
+console.log("First Element is : "+Arr[0]);
+console.log("Second Element of Array is : "+Arr[1]);
+
+console.log("Elements of Array are =>");
+var i:number=0;
+
+for(i=0;i<Arr.length;i++)
+{
+    console.log("\t\t\t"+Arr[i]);
+}
